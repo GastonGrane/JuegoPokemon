492a4b0738f7684f2bc984f66333b00c9a9d4876
Axel Hernandez <axel.hernandez@correo.ucu.edu.uy>
2024-10-10 23:38:59 -0300

    aporto atack, ipokemon, elements, pikachu

--- src/Library/Pokemons/IPokemon.cs
+++ src/Library/Pokemons/IPokemon.cs
@@ -1,6 +1,12 @@
 namespace DefaultNamespace;
 
-public class IPokemon
+public interface IPokemon
 {
+    public string Name { get; set; }
+    public IElement Element;
+    public int Health;
+    public int MaxHealth;
+    public Attacks[] attacks;
     
+    void Attack(IPokemon target, Attacks ataq);
 }
--- src/Library/Pokemons/Pikachu.cs
+++ src/Library/Pokemons/Pikachu.cs
@@ -1,6 +1,24 @@
 namespace DefaultNamespace;
 
-public class Pikachu
+public class Pikachu : IPokemon
 {
-    
+    public string Name;
+    public IElement Element;
+    public int Health;
+    public int MaxHealth;
+    private readonly object IElement;
+    public Attacks[] attacks;
+
+    public  Pikachu()
+    {
+        this.Name = "Pikachu";
+        this.IElement = typeof(Electrico);
+        this.Health = 100;
+        this.MaxHealth = 10;
+    }
+
+    public void Attack(IPokemon target)
+    {
+        Attack = 
+    }
 }
