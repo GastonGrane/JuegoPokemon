9f697a58c37d73edbc892082881ca7b866f9b025
axelher006 <axel.hernandez@correo.ucu.edu.uy>
2024-10-10 23:41:57 -0300

    Update Pikachu.cs

--- src/Library/Pokemons/Pikachu.cs
+++ src/Library/Pokemons/Pikachu.cs
@@ -1,24 +1,48 @@
-namespace DefaultNamespace;
+using System;
+using System.Collections.Generic;
 
-public class Pikachu : IPokemon
+namespace DefaultNamespace
 {
-    public string Name;
-    public IElement Element;
-    public int Health;
-    public int MaxHealth;
-    private readonly object IElement;
-    public Attacks[] attacks;
-
-    public  Pikachu()
+    public class Pikachu : IPokemon
     {
-        this.Name = "Pikachu";
-        this.IElement = typeof(Electrico);
-        this.Health = 100;
-        this.MaxHealth = 10;
-    }
+        public string Name { get; set; }
+        public Element Element { get; set; }
+        public int Health { get; set; }
+        public int MaxHealth { get; set; }
+        public List<Attack> Attacks { get; set; }
 
-    public void Attack(IPokemon target)
-    {
-        Attack = 
+        public Pikachu()
+        {
+            this.Name = "Pikachu";
+            this.Element = Element.Electric;
+            this.Health = 100;
+            this.MaxHealth = 100;
+            this.Attacks = new List<Attack>()
+            {
+                new Attack("Trueno", 50)
+            };
+        }
+
+        public void Attack(IPokemon target, string attackName)
+        {
+            attackName = attackName.ToLower();
+            if (!attacks.Countains(attack))
+            {
+                return;
+            }
+
+            posicion = attacks.index(attack);
+            defender = target.Element;
+            attacker = this.Element;
+
+            double multiplier = Calculate.Advantage(attacker, defender);
+            int damage = (int)(attack[posicion].Damage * multiplier);
+
+            target.Health -= damage;
+            if (target.Health < 0)
+            {
+                target.Health = 0;
+            }
+        }
     }
 }
