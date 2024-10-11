9fb4beae48b459aa6955a203c2bfb9543fc71545
Axel Hernandez <axel.hernandez@correo.ucu.edu.uy>
2024-10-10 21:00:02 -0300

    vamo a hacer algo

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,9 +1,41 @@
-﻿namespace Program;
-
-class Program
+﻿public interface IPokemon
 {
-    static void Main(string[] args)
+    public string Name { get; set; }
+    public IElement Element;
+    public int Health;
+    public int MaxHealth;
+    public Attacks[] attacks;
+    
+    void Attack(IPokemon target, Attacks ataq);
+}
+
+//-----------------------------------------
+public class Attacks
+{
+}
+
+
+//--------------------------------------------
+
+public class Pikachu : IPokemon
+{
+    public string Name;
+    public IElement Element;
+    public int Health;
+    public int MaxHealth;
+    private readonly object IElement;
+    public Attacks[] attacks;
+
+    public  Pikachu()
     {
-        Console.WriteLine("Hello, World!");
+        this.Name = "Pikachu";
+        this.IElement = typeof(Electrico);
+        this.Health = 100;
+        this.MaxHealth = 10;
+    }
+
+    public void Attack(IPokemon target)
+    {
+        Attack = 
     }
 }
