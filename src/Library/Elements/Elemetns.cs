198be8480d9ee67326d64723dfeaa8be9ba9e362
axelher006 <axel.hernandez@correo.ucu.edu.uy>
2024-10-10 23:40:10 -0300

    Update Elemetns.cs

--- src/Library/Elements/Elemetns.cs
+++ src/Library/Elements/Elemetns.cs
@@ -1,6 +1,43 @@
 namespace DefaultNamespace;
 
-public class Elemetns
+public enum Element
 {
-    
+    Fire,
+    Water,
+    Grass
+}
+
+public class Calculate
+{
+    public static double Advantage(Element attacker, Element defender)
+    {
+        if (attacker == Element.Fire && defender == Element.Water)
+        {
+            return 2.0;
+        }
+        else if (attacker == Element.Water && defender == Element.Grass)
+        {
+            return 2.0;
+        }
+        else if (attacker == Element.Grass && defender == Element.Fire)
+        {
+            return 2.0;
+        }
+        else if (attacker == Element.Water && defender == Element.Fire)
+        {
+            return 0.5;
+        }
+        else if (attacker == Element.Grass && defender == Element.Water)
+        {
+            return 0.5;
+        }
+        else if (attacker == Element.Fire && defender == Element.Grass)
+        {
+            return 0.5;
+        }
+        else
+        {
+            return null;
+        }
+    }
 }
