84afbf302c65ff671bf54ad3b9dec0ee79abae4f
axelher006 <axel.hernandez@correo.ucu.edu.uy>
2024-10-10 23:40:39 -0300

    Update Atacks.cs

--- src/Library/Attacks/Atacks.cs
+++ src/Library/Attacks/Atacks.cs
@@ -1,6 +1,14 @@
-namespace DefaultNamespace;
-
-public class Atacks
+namespace DefaultNamespace
 {
-    
+    public class Attack
+    {
+        public string Name { get; set; }
+        public int Damage { get; set; }
+
+        public Attack(string name, int damage)
+        {
+            this.Name = name.ToLower();
+            this.Damage = damage;
+        }
+    }
 }
