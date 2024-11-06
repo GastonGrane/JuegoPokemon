c053272fbe1ffe62f45283c3e78afc681e0ccbaa
axelher006 <axel.hernandez@correo.ucu.edu.uy>
2024-11-06 20:37:10 -0300

    Arreglo y documento main (#5)
    Co-authored-by: Guzmán Zugnoni <guzman.zugnoni@correo.ucu.edu.uy>

--- src/Library/Print/IPrint.cs
+++ src/Library/Print/IPrint.cs
@@ -2,9 +2,9 @@ namespace Library;
 
 public interface IPrint
 {
-    public void printList(List<Pokemon> list);
-    public void printString(string str);
-    public int printStringAndReceiveInt(string str);
+    public void PrintList(List<Pokemon> list);
+    public void PrintString(string str);
+    public int PrintStringAndReceiveInt(string str);
     public void PrintListAtaque(List<Attack> ataques);
     public int SelectAtaque(string str);
 
