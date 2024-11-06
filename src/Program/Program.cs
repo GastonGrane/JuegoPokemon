c7a6d62391d93a9c105bde84468d2de39f45fbf7
Axel Hernandez <axel.hernandez@correo.ucu.edu.uy>
2024-11-06 20:17:36 -0300

    Arreglo y documento main

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,28 +1,63 @@
 ﻿using Library;
 
-namespace Program;
-
-class Program
+namespace Program
 {
-    public static void Main(string[] args)
+    /// <summary>
+    /// Clase principal que ejecuta el programa para crear y mostrar una lista de Pokémon en la consola.
+    /// </summary>
+    class Program
     {
-        Pokemon pokemon1 = new Pokemon("Pikachu", PokemonType.Electric, 100,
-            new List<Attack> { NormalAttack.FusionBolt });
-        Pokemon pokemon2 = new Pokemon("Bulbasaur", PokemonType.Fire, 100,
-            new List<Attack>() { NormalAttack.BulletSeed });
-        Pokemon pokemon3 = new Pokemon("Charmander", PokemonType.Fire, 100,
-            new List<Attack>() { NormalAttack.BlazeKick });
-        Pokemon pokemon4 = new Pokemon("Squirtle", PokemonType.Water, 100,
-            new List<Attack>() { NormalAttack.AquaJet, });
-        List<Pokemon> Pokemons = new List<Pokemon>();
-        Pokemons.Add(pokemon1);
-        Pokemons.Add(pokemon2);
-        Pokemons.Add(pokemon3);
-        Pokemons.Add(pokemon4);
-        PrintConsole.printList(Pokemons);
+        /// <summary>
+        /// Punto de entrada principal del programa.
+        /// </summary>
+        /// <param name="args">Argumentos de la línea de comandos.</param>
+        public static void Main(string[] args)
+        {
+            PrintConsole printConsole = new PrintConsole();
+
+            /// <summary>
+            /// Crea una instancia de Pikachu, un Pokémon de tipo Eléctrico, con 100 puntos de vida
+            /// y el ataque FusionBolt.
+            /// </summary>
+            Pokemon pokemon1 = new Pokemon("Pikachu", PokemonType.Electric, 100,
+                new List<Attack> { NormalAttack.FusionBolt });
+
+            /// <summary>
+            /// Crea una instancia de Bulbasaur, un Pokémon de tipo Fuego, con 100 puntos de vida
+            /// y el ataque BulletSeed.
+            /// </summary>
+            Pokemon pokemon2 = new Pokemon("Bulbasaur", PokemonType.Fire, 100,
+                new List<Attack> { NormalAttack.BulletSeed });
+
+            /// <summary>
+            /// Crea una instancia de Charmander, un Pokémon de tipo Fuego, con 100 puntos de vida
+            /// y el ataque BlazeKick.
+            /// </summary>
+            Pokemon pokemon3 = new Pokemon("Charmander", PokemonType.Fire, 100,
+                new List<Attack> { NormalAttack.BlazeKick });
+
+            /// <summary>
+            /// Crea una instancia de Squirtle, un Pokémon de tipo Agua, con 100 puntos de vida
+            /// y el ataque AquaJet.
+            /// </summary>
+            Pokemon pokemon4 = new Pokemon("Squirtle", PokemonType.Water, 100,
+                new List<Attack> { NormalAttack.AquaJet });
+
+            /// <summary>
+            /// Lista de Pokémon creados para ser mostrados en la consola.
+            /// </summary>
+            List<Pokemon> Pokemons = new List<Pokemon>
+            {
+                pokemon1,
+                pokemon2,
+                pokemon3,
+                pokemon4
+            };
+
+            /// <summary>
+            /// Imprime la lista de Pokémon en la consola usando el método printList de la clase PrintConsole.
+            /// </summary>
+            printConsole.printList(Pokemons);
+        }
     }
 }
-
-
-//Game game = Game.createGame([]);
-//game.Play();
