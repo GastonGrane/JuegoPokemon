c5ac29c518b64ae67ab4994e697ae03c4d1b0ec1
Gaston <gaston.grane@correo.ucu.edu.uy>
2024-11-18 20:30:44 -0300

    Fix problem with copyright

--- src/Library/GameLogic/Utilities/SystemRandom.cs
+++ src/Library/GameLogic/Utilities/SystemRandom.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="AleatoriedadPrograma.cs" company="Universidad Católica del Uruguay">
+// <copyright file="SystemRandom.cs" company="Universidad Católica del Uruguay">
 // Copyright (c) Programación II. Derechos reservados.
 // </copyright>
 // -----------------------------------------------------------------------
